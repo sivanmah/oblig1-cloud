3ef79f3e8be6def6c841491c14a1fbad51999824
sivanmah <sivansm@stud.ntnu.no>
2022-03-04 14:54:09 +0100

    set up basic file structure

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'public','index.html'));
+});
